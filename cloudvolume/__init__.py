da1a253644bcb810ad8e389b9f1ba0e99ec3566c
William Silversmith <william.silversmith@gmail.com>
2017-09-20 21:20:39 -0400

    fix: missing module secrets (#26)
    Needed to export secrets from the package in __init__

--- cloudvolume/__init__.py
+++ cloudvolume/__init__.py
@@ -2,3 +2,4 @@ from .cloudvolume import CloudVolume, EmptyVolumeException
 from .provenance import DataLayerProvenance
 from .storage import Storage
 from .threaded_queue import ThreadedQueue
+import secrets
