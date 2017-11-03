0cb5da2402f76d472bc95ccb147b40cfd6237d38
William Silversmith <william.silversmith@gmail.com>
2017-11-03 05:01:26 -0400

    feat: backwards compatible secrets (#57)
    * feat: backwards compatible secrets
    
    * feat: additional options for specifying project_name
    
    1) Enable environment variable override
    2) Make it easier to create a kubernetes secrets volume that contains the project_name

--- cloudvolume/secrets.py
+++ cloudvolume/secrets.py
@@ -7,40 +7,58 @@ from google.oauth2 import service_account
 
 from .lib import mkdir, colorize
 
-backwards_compatible_path = os.path.join(os.environ['HOME'], '.neuroglancer/')
-new_path = os.path.join(os.environ['HOME'], '.cloudvolume/')
+CLOUD_VOLUME_DIR = mkdir(os.path.join(os.environ['HOME'], '.cloudvolume'))
 
-if os.path.exists(new_path):
-  CLOUD_VOLUME_DIR = new_path
-elif os.path.exists(backwards_compatible_path):
-  CLOUD_VOLUME_DIR = backwards_compatible_path
-  print(colorize('yellow', 'Deprecation Warning: Directory ~/.cloudvolume is now preferred to ~/.neuroglancer.\nConsider running: mv ~/.neuroglancer ~/.cloudvolume'))
-else:
-  CLOUD_VOLUME_DIR = mkdir(new_path)
+def secretpath(filepath):
+  preferred = os.path.join(CLOUD_VOLUME_DIR, filepath)
+  
+  if os.path.exists(preferred):
+    return preferred
 
-secret_path = mkdir(os.path.join(CLOUD_VOLUME_DIR, 'secrets/'))
+  backcompat = [
+    os.path.join(os.environ['HOME'], '.neuroglancer'), # older
+    '/' # original
+  ]
 
-PROJECT_NAME = None
-project_name_path = os.path.join(CLOUD_VOLUME_DIR, 'project_name')
-if os.path.exists(project_name_path):
-  with open(project_name_path, 'r') as f:
-    PROJECT_NAME = f.read()
+  backcompat = [ os.path.join(path, filepath) for path in backcompat ] 
 
-google_credentials_path = os.path.join(secret_path, 'google-secret.json')
+  for path in backcompat:
+    if os.path.exists(path):
+      print(colorize('yellow', 'Deprecation Warning: {} is now preferred to {}.'.format(preferred, path)))  
+      return path
+
+  return preferred
+
+PROJECT_NAME = None 
+project_name_paths = [ 
+  secretpath('secrets/project_name'),
+  secretpath('project_name')
+]
+
+if 'GOOGLE_PROJECT_NAME' in os.environ: 
+  PROJECT_NAME = os.environ['GOOGLE_PROJECT_NAME']
+else: 
+  for path in project_name_paths:
+    if os.path.exists(path):
+      with open(path, 'r') as f:
+        PROJECT_NAME = f.read().strip()
+      break
+
+google_credentials_path = secretpath('secrets/google-secret.json')
 if os.path.exists(google_credentials_path):
   google_credentials = service_account.Credentials \
     .from_service_account_file(google_credentials_path)
 else:
   google_credentials = ''
 
-aws_credentials_path = os.path.join(secret_path, 'aws-secret.json')
+aws_credentials_path = secretpath('secrets/aws-secret.json')
 if os.path.exists(aws_credentials_path):
   with open(aws_credentials_path, 'r') as f:
     aws_credentials = json.loads(f.read())
 else:
   aws_credentials = ''
 
-boss_credentials_path = os.path.join(secret_path, 'boss-secret.json')
+boss_credentials_path = secretpath('secrets/boss-secret.json')
 if os.path.exists(boss_credentials_path):
   with open(boss_credentials_path, 'r') as f:
     boss_credentials = json.loads(f.read())
