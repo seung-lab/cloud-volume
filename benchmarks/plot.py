80b260f123d4e389f4d6035b773e615f665a6a8e
William Silversmith <william.silversmith@gmail.com>
2018-02-09 19:34:44 -0500

    docs: benchmark data and plotting for gcloud

---
+++ benchmarks/plot.py
@@ -0,0 +1,66 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+
+import sys
+from collections import defaultdict
+
+rows = []
+
+filename = sys.argv[1] 
+
+with open(filename, 'r') as csvfile:
+  votereader = csv.reader(csvfile, delimiter='\t', quotechar=None)
+  for row in votereader:
+        rows.append(row)
+
+headers = rows.pop(0)
+
+stats = defaultdict(lambda: defaultdict(lambda: defaultdict(dict)))
+
+for row in rows:
+	direction, imgtype, compression, MB, meanMBs = row[1], row[3], row[2], row[6], row[7]
+	stats[direction][compression][imgtype][float(MB)] = float(meanMBs)
+
+
+styles = {
+	'black': 'b.',
+	'image': 'r.',
+	'segmentation': 'g.',
+}
+
+
+index = 0
+for direction in stats.keys():
+	direxpr = stats[direction]
+
+	for compress in direxpr.keys():
+		imageexper = direxpr[compress]
+		index += 1
+
+		plt.subplot(2, 2, index)
+		plt.title(filename + ': CloudVolume ' + direction + ' Speed w/ ' + compress + ' Compression')
+		plt.xlabel('Chunk Size (MB)')
+		plt.ylabel('MB/sec')
+
+		lines = []
+		legends = []
+
+		for imgtype in imageexper.keys():
+			style = styles[imgtype]
+			experiment = imageexper[imgtype]
+			xdata = [ float(mb) for mb in experiment.keys() ]
+			xdata.sort()
+			ydata = [ float(experiment[mb]) for mb in xdata ]
+			line = plt.plot(xdata, ydata, style, linestyle='-', linewidth=2)
+			lines.append(line)
+			legends.append(imgtype)
+
+		plt.legend(legends)
+plt.show()
+
+
+
+
+
+
