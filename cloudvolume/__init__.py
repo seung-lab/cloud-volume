d266c53b52eca75df9d701e16bcdfd9fe2300544
William Silversmith <william.silversmith@gmail.com>
2018-02-27 19:47:19 -0500

    feat: exported ConnectionPools

--- cloudvolume/__init__.py
+++ cloudvolume/__init__.py
@@ -2,5 +2,6 @@ from .cloudvolume import CloudVolume, EmptyVolumeException, EmptyRequestExceptio
 from .provenance import DataLayerProvenance
 from .storage import Storage
 from .threaded_queue import ThreadedQueue
+from .connectionpools import ConnectionPool
 
 from . import secrets
