81ed196bbd46cb86846cb5b7651e8cd65b0746dd
William Silversmith <william.silversmith@gmail.com>
2018-04-10 17:34:22 -0400

    feat: only allocate shared memory when memory is available

--- cloudvolume/sharedmemory.py
+++ cloudvolume/sharedmemory.py
@@ -1,13 +1,18 @@
-import posix_ipc
-from posix_ipc import O_CREAT
+import errno
 import mmap
 import os
 
+import posix_ipc
+from posix_ipc import O_CREAT
 import numpy as np
+import psutil
 
 from .lib import Bbox, Vec
 
 mmaps = []
+
+class MemoryAllocationError(Exception):
+  pass
 
 def reinit():
   """For use after a process fork only. Trashes bad file descriptors and resets tracking."""
@@ -20,10 +25,43 @@ def bbox2array(vol, bbox):
 
 def ndarray(shape, dtype, location):
   nbytes = Vec(*shape).rectVolume() * np.dtype(dtype).itemsize
-  shared = posix_ipc.SharedMemory(location, flags=O_CREAT, size=int(nbytes))
-  array_like = mmap.mmap(shared.fd, shared.size)
-  os.close(shared.fd)
-  renderbuffer = np.ndarray(buffer=array_like, dtype=dtype, shape=shape)
+  available = psutil.virtual_memory().available
+
+  preexisting = 0
+  # This might only work on Ubuntu
+  shmloc = os.path.join('/dev/shm/', location)
+  if os.path.exists(shmloc):
+    preexisting = os.path.getsize(shmloc)
+
+  if (nbytes - preexisting) > available:
+    overallocated = nbytes - preexisting - available
+    overpercent = (100 * overallocated / (preexisting + available))
+    raise MemoryAllocationError("""
+      Requested more memory than is available. 
+
+      Shared Memory Location:  {}
+
+      Shape:                   {}
+      Requested Bytes:         {} 
+      
+      Available Bytes:         {} 
+      Preexisting Bytes*:      {} 
+
+      Overallocated Bytes*:    {} (+{:.2f}%)
+
+      * Preexisting is only correct on linux systems that support /dev/shm/""" \
+        .format(location, shape, nbytes, available, preexisting, overallocated, overpercent))
+
+  try:
+    shared = posix_ipc.SharedMemory(location, flags=O_CREAT, size=int(nbytes))
+    array_like = mmap.mmap(shared.fd, shared.size)
+    os.close(shared.fd)
+    renderbuffer = np.ndarray(buffer=array_like, dtype=dtype, shape=shape)
+  except OSError as err:
+    if err.errno == errno.ENOMEM: # Out of Memory
+      posix_ipc.unlink_shared_memory(location)      
+    raise
+
   return array_like, renderbuffer
 
 def track_mmap(array_like):
