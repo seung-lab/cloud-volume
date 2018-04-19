bc69990364be96d673ffaa91b5d616ce46b00b86
William Silversmith <william.silversmith@gmail.com>
2018-04-19 04:59:06 -0400

    fix: install numpy before assuming numpy

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 import os
 import setuptools
+from setuptools.command.build_ext import build_ext as _build_ext
 
-import numpy as np
+class build_ext(_build_ext):
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process:
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
 
 compresso_cpp = './ext/third_party/compresso/c++/'
 compresso_py = './ext/third_party/compresso/python/'
 
 setuptools.setup(
+    cmdclass={'build_ext':build_ext},
     setup_requires=['pbr', 'numpy'],
     pbr=True,
     ext_modules=[
