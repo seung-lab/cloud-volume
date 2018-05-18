68ff0de95177167184c2370cca3482d26dcc2cd8
William Silversmith <william.silversmith@gmail.com>
2018-05-18 13:26:48 -0400

    fix: make futures conditional on python 2 (#110)

--- setup.py
+++ setup.py
@@ -2,4 +2,8 @@ import setuptools
 
 setuptools.setup(
     setup_requires=['pbr'],
+    extras_require={
+    	':python_version == "2.7"': ['futures'],
+    	':python_version == "2.6"': ['futures'],
+    },
     pbr=True)
