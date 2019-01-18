9aa4a6d5cabb9d7c271116b9f54b15214c8237be
Forrest Collman <forrest.collman@gmail.com>
2019-01-18 15:04:39 -0800

    stub factory

---
+++ cloudvolume/cloud_factory.py
@@ -0,0 +1,14 @@
+
+def CloudFactory(protocol):
+
+    
+    class MetaCloudVolume(CloudVolume):
+
+class CloudFactory(object):
+
+    def __init__(self, protocol_path):
+
+        # split the protocol_path into protocol and path
+
+        # 
+
