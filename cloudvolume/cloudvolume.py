fc7f3e766e5da54602d72bcf6edabb4032a7ba97
jingpengw <jingpeng.wu@gmail.com>
2019-02-11 21:43:41 -0500

    bool should be segmentation rather than image

--- cloudvolume/cloudvolume.py
+++ cloudvolume/cloudvolume.py
@@ -201,10 +201,10 @@ class CloudVolume(object):
     the numpy array itself was not downsampled. 
     """
     if not layer_type:
-      if arr.dtype in (np.uint32, np.uint64, np.uint16):
+      if arr.dtype in (np.bool, np.uint32, np.uint64, np.uint16):
         layer_type = 'segmentation'
-      elif arr.dtype==np.bool or np.issubdtype(arr.dtype, np.integer) \
-                                or np.issubdtype(arr.dtype, np.floating):
+      elif np.issubdtype(arr.dtype, np.integer) \
+                        or np.issubdtype(arr.dtype, np.floating):
         layer_type = 'image'
       else:
         raise NotImplementedError
