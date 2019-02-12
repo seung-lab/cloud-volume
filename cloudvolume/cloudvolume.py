3725d3b0352780d5648c1ffdaaea551939b98997
jingpengw <jingpeng.wu@gmail.com>
2019-02-11 21:28:32 -0500

    add boolean and use floating abstract dtype

--- cloudvolume/cloudvolume.py
+++ cloudvolume/cloudvolume.py
@@ -203,7 +203,8 @@ class CloudVolume(object):
     if not layer_type:
       if arr.dtype in (np.uint32, np.uint64, np.uint16):
         layer_type = 'segmentation'
-      elif np.issubdtype(arr.dtype, np.integer) or np.issubdtype(arr.dtype, np.float):
+      elif arr.dtype is np.bool or np.issubdtype(arr.dtype, np.integer) \
+                                or np.issubdtype(arr.dtype, np.floating):
         layer_type = 'image'
       else:
         raise NotImplementedError
