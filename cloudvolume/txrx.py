30b812df99d165b0968157c30a707cc853dc243a
William Silversmith <william.silversmith@gmail.com>
2019-04-10 22:54:59 -0400

    wip: delete black uploads flag

--- cloudvolume/txrx.py
+++ cloudvolume/txrx.py
@@ -270,8 +270,12 @@ def check_grid_aligned(vol, img, offset):
   is_aligned = np.all(alignment_check.minpt == bounds.minpt) and np.all(alignment_check.maxpt == bounds.maxpt)
   return (is_aligned, bounds, alignment_check) 
 
-def upload_image(vol, img, offset, parallel=1, 
-  manual_shared_memory_id=None, manual_shared_memory_bbox=None, manual_shared_memory_order='F'):
+def upload_image(
+    vol, img, offset, 
+    parallel=1, 
+    manual_shared_memory_id=None, manual_shared_memory_bbox=None, 
+    manual_shared_memory_order='F', delete_black_uploads=False
+  ):
   """Upload img to vol with offset. This is the primary entry point for uploads."""
   global NON_ALIGNED_WRITE
 
@@ -281,9 +285,14 @@ def upload_image(vol, img, offset, parallel=1,
   (is_aligned, bounds, expanded) = check_grid_aligned(vol, img, offset)
 
   if is_aligned:
-    upload_aligned(vol, img, offset, parallel=parallel, 
-      manual_shared_memory_id=manual_shared_memory_id, manual_shared_memory_bbox=manual_shared_memory_bbox,
-      manual_shared_memory_order=manual_shared_memory_order)
+    upload_aligned(
+      vol, img, offset, 
+      parallel=parallel, 
+      manual_shared_memory_id=manual_shared_memory_id, 
+      manual_shared_memory_bbox=manual_shared_memory_bbox,
+      manual_shared_memory_order=manual_shared_memory_order,
+      delete_black_uploads=delete_black_uploads
+    )
     return
   elif vol.non_aligned_writes == False:
     msg = NON_ALIGNED_WRITE.format(mip=vol.mip, chunk_size=vol.chunk_size, offset=vol.voxel_offset, got=bounds, check=expanded)
@@ -295,9 +304,14 @@ def upload_image(vol, img, offset, parallel=1,
 
   if not core_bbox.subvoxel():
     core_img = img[ core_bbox.to_slices() ] 
-    upload_aligned(vol, core_img, retracted.minpt, parallel=parallel, 
-      manual_shared_memory_id=manual_shared_memory_id, manual_shared_memory_bbox=manual_shared_memory_bbox,
-      manual_shared_memory_order=manual_shared_memory_order)
+    upload_aligned(
+      vol, core_img, retracted.minpt, 
+      parallel=parallel, 
+      manual_shared_memory_id=manual_shared_memory_id, 
+      manual_shared_memory_bbox=manual_shared_memory_bbox,
+      manual_shared_memory_order=manual_shared_memory_order, 
+      delete_black_uploads=delete_black_uploads
+    )
 
   # Download the shell, paint, and upload
   all_chunks = set(chunknames(expanded, vol.bounds, vol.key, vol.underlying))
@@ -313,8 +327,12 @@ def upload_image(vol, img, offset, parallel=1,
 
   download_multiple(vol, shell_chunks, fn=shade_and_upload)
 
-def upload_aligned(vol, img, offset, parallel=1, 
-  manual_shared_memory_id=None, manual_shared_memory_bbox=None, manual_shared_memory_order='F'):
+def upload_aligned(
+    vol, img, offset, 
+    parallel=1, 
+    manual_shared_memory_id=None, manual_shared_memory_bbox=None, 
+    manual_shared_memory_order='F', delete_black_uploads=False
+  ):
   global fs_lock
 
   chunk_ranges = list(generate_chunks(vol, img, offset))
@@ -334,12 +352,18 @@ def upload_aligned(vol, img, offset, parallel=1,
     shared_memory_id = manual_shared_memory_id
   else:
     shared_memory_id = vol.shared_memory_id
-    array_like, renderbuffer = shm.ndarray(shape=img.shape, dtype=img.dtype, 
-      location=shared_memory_id, order=manual_shared_memory_order, lock=fs_lock)
+    array_like, renderbuffer = shm.ndarray(
+      shape=img.shape, dtype=img.dtype, 
+      location=shared_memory_id, order=manual_shared_memory_order, 
+      lock=fs_lock
+    )
     renderbuffer[:] = img
 
-  mpu = partial(multi_process_upload, vol, img.shape, offset, 
-    shared_memory_id, manual_shared_memory_bbox, manual_shared_memory_order, vol.cache.enabled)
+  mpu = partial(multi_process_upload, 
+    vol, img.shape, offset, 
+    shared_memory_id, manual_shared_memory_bbox, manual_shared_memory_order, 
+    vol.cache.enabled
+  )
 
   cleanup_shm = shared_memory_id if not manual_shared_memory_id else None
   parallel_execution(mpu, chunk_ranges_by_process, parallel, cleanup_shm=cleanup_shm)
@@ -371,7 +395,11 @@ def multi_process_upload(vol, img_shape, offset,
   single_process_upload(vol, renderbuffer, chunk_ranges)
   array_like.close()
 
-def single_process_upload(vol, img, chunk_ranges, n_threads=DEFAULT_THREADS):
+def single_process_upload(
+    vol, img, chunk_ranges, 
+    n_threads=DEFAULT_THREADS,
+    delete_black_uploads=False
+  ):
   if vol.cache.enabled:
     mkdir(vol.cache.path)
     if vol.progress:
@@ -381,8 +409,32 @@ def single_process_upload(vol, img, chunk_ranges, n_threads=DEFAULT_THREADS):
   cloudstorage = Storage(vol.layer_cloudpath, progress=vol.progress, n_threads=n_threads)
   iterator = tqdm(chunk_ranges, desc='Rechunking image', disable=(not vol.progress))
 
-  if len(img.shape) == 3:
-    img = img[:, :, :, np.newaxis ]
+  while img.ndim < 4:
+    img = img[ ..., np.newaxis ]
+
+  def do_upload(imgchunk, cloudpath):
+    encoded = chunks.encode(imgchunk, vol.encoding, vol.compressed_segmentation_block_size)
+
+    cloudstorage.put_file(
+      file_path=cloudpath, 
+      content=encoded,
+      content_type=content_type(vol), 
+      compress=should_compress(vol),
+      cache_control=cdn_cache_control(vol.cdn_cache),
+    )
+
+    if vol.cache.enabled:
+      cachestorage.put_file(
+        file_path=cloudpath,
+        content=encoded, 
+        content_type=content_type(vol), 
+        compress=should_compress(vol, iscache=True)
+      )
+
+  def do_delete(cloudpath):
+    cloudstorage.delete_file(cloudpath)
+    if vol.cache.enabled:
+      cachestorage.delete_file(cloudpath)
 
   for startpt, endpt, spt, ept in iterator:
     if np.array_equal(spt, ept):
@@ -402,23 +454,14 @@ def single_process_upload(vol, img, chunk_ranges, n_threads=DEFAULT_THREADS):
     )
 
     cloudpath = os.path.join(vol.key, filename)
-    encoded = chunks.encode(imgchunk, vol.encoding, vol.compressed_segmentation_block_size)
 
-    cloudstorage.put_file(
-      file_path=cloudpath, 
-      content=encoded,
-      content_type=content_type(vol), 
-      compress=should_compress(vol),
-      cache_control=cdn_cache_control(vol.cdn_cache),
-    )
-
-    if vol.cache.enabled:
-      cachestorage.put_file(
-        file_path=cloudpath,
-        content=encoded, 
-        content_type=content_type(vol), 
-        compress=should_compress(vol, iscache=True)
-      )
+    if delete_black_uploads:
+      if np.any(imgchunk):
+        do_upload(imgchunk, cloudpath)
+      else:
+        do_delete(cloudpath)
+    else:
+      do_upload(imgchunk, cloudpath)
 
   desc = 'Uploading' if vol.progress else None
   cloudstorage.wait(desc)
