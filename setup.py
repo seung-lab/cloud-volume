7602dc8e05f78e776b7f683cbf6020ddb0c04e42
William Silversmith <william.silversmith@gmail.com>
2019-08-02 20:47:48 -0400

    chore: make pytest optional with [test] dependency (#255)
    Resolves #252

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setuptools.setup(
     "mesh_viewer": [ 'vtk' ],
     "skeleton_viewer": [ 'matplotlib' ],
     "all_viewers": [ 'vtk', 'matplotlib' ],
+    "test": [ "pytest" ],
   },
   author="William Silversmith, Nico Kemnitz, Ignacio Tartavull, and others",
   author_email="ws9@princeton.edu",
