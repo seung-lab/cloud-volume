c1ae9689aebce6abceafbe05993a47ebd2169752
William Silversmith <william.silversmith@gmail.com>
2019-08-15 22:39:51 -0400

    docs: reference for paper defining SWC format

--- cloudvolume/skeleton.py
+++ cloudvolume/skeleton.py
@@ -900,6 +900,21 @@ class Skeleton(object):
 
   @classmethod
   def from_swc(self, swcstr):
+    """
+    The SWC format was first defined in 
+    
+    R.C Cannona, D.A Turner, G.K Pyapali, H.V Wheal. 
+    "An on-line archive of reconstructed hippocampal neurons".
+    Journal of Neuroscience Methods
+    Volume 84, Issues 1–2, 1 October 1998, Pages 49-54
+    doi: 10.1016/S0165-0270(98)00091-0
+
+    This website is also helpful for understanding the format:
+
+    https://web.archive.org/web/20180423163403/http://research.mssm.edu/cnic/swc.html
+
+    Returns: Skeleton
+    """
     lines = swcstr.split("\n")
     while lines[0] == '' or re.match(r'[#\s]', lines[0][0]):
       lines.pop(0)
@@ -917,7 +932,6 @@ class Skeleton(object):
     for i, line in enumerate(lines):
       if line.replace(r"\s", '') == '':
         continue
-
       (vid, vtype, x, y, z, radius, parent_id) = line.split(" ")
       
       coord = tuple([ float(_) for _ in (x,y,z) ])
@@ -953,7 +967,19 @@ class Skeleton(object):
     """
     Prototype SWC file generator. 
 
-    c.f. http://research.mssm.edu/cnic/swc.html
+    The SWC format was first defined in 
+    
+    R.C Cannona, D.A Turner, G.K Pyapali, H.V Wheal. 
+    "An on-line archive of reconstructed hippocampal neurons".
+    Journal of Neuroscience Methods
+    Volume 84, Issues 1–2, 1 October 1998, Pages 49-54
+    doi: 10.1016/S0165-0270(98)00091-0
+
+    This website is also helpful for understanding the format:
+
+    https://web.archive.org/web/20180423163403/http://research.mssm.edu/cnic/swc.html
+
+    Returns: swc as a string
     """
     from . import __version__
     swc_header = """# ORIGINAL_SOURCE CloudVolume {}
