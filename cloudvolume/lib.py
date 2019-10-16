79c8f10f78471231ca09a74da7821d01104729b2
William Silversmith <william.silversmith@gmail.com>
2019-10-16 16:04:34 -0400

    feat: jsonify can parse numpy integer types

--- cloudvolume/lib.py
+++ cloudvolume/lib.py
@@ -41,6 +41,10 @@ class NumpyEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, np.ndarray):
             return obj.tolist()
+        if isinstance(obj, np.integer):
+            return int(obj)
+        if isinstance(obj, np.floating):
+            return float(obj)
         return json.JSONEncoder.default(self, obj)
 
 def toiter(obj):
