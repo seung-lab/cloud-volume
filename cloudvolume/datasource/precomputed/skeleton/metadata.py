ce695105876efb595f9aa82c224ade16630a653a
William Silversmith <william.silversmith@gmail.com>
2019-10-21 19:37:00 -0400

    fix: use jsonify for skeleton info file
    Allows the use of numpy types in numbers.

--- cloudvolume/datasource/precomputed/skeleton/metadata.py
+++ cloudvolume/datasource/precomputed/skeleton/metadata.py
@@ -1,4 +1,4 @@
-import json
+from ....lib import jsonify
 
 import numpy as np
 
@@ -61,7 +61,7 @@ class PrecomputedSkeletonMetadata(object):
     if self.info:
       self.cache.upload_single(
         self.meta.join(self.skeleton_path, 'info'),
-        json.dumps(self.info), 
+        jsonify(self.info), 
         content_type='application/json',
         compress=False,
         cache_control='no-cache',
