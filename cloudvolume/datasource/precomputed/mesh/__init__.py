87d51977cce7967bbaeb0a45c869dc2f70b3ee9a
William Silversmith <william.silversmith@gmail.com>
2019-11-25 19:04:30 -0500

    feat: read mesh spatial index (#286)
    * feat: read mesh spatial index
    
    feat: create mesh metadata class
    
    refactor: structure mesh datasources to accomodate sharded
    
    * feat: add is_multires and is_legacy to mesh metadata

---
+++ cloudvolume/datasource/precomputed/mesh/__init__.py
@@ -0,0 +1,42 @@
+import os
+
+from .metadata import PrecomputedMeshMetadata
+from .unsharded import UnshardedLegacyPrecomputedMeshSource
+
+from ..metadata import PrecomputedMetadata
+from ....cacheservice import CacheService
+from ....paths import strict_extract
+from ....cloudvolume import SharedConfiguration
+
+class PrecomputedMeshSource(object):
+  def __new__(cls, meta, cache, config, readonly=False):
+    mesh_meta = PrecomputedMeshMetadata(meta, cache)
+
+    # if mesh_meta.is_sharded():
+    #   return ShardedLegacyPrecomputedMeshSource(mesh_meta, cache, config, readonly) 
+
+    return UnshardedLegacyPrecomputedMeshSource(mesh_meta, cache, config, readonly)
+
+  @classmethod
+  def from_cloudpath(cls, cloudpath, cache=False, progress=False):
+    config = SharedConfiguration(
+      cdn_cache=False,
+      compress=True,
+      compress_level=None,
+      green=False,
+      mip=0,
+      parallel=1,
+      progress=progress,
+    )
+
+    cache = CacheService(
+      cloudpath=(cache if type(cache) == str else cloudpath),
+      enabled=bool(cache),
+      config=config,
+      compress=True,
+    )
+
+    cloudpath, mesh_dir = os.path.split(cloudpath)
+    meta = PrecomputedMetadata(cloudpath, cache, info={ 'mesh': mesh_dir })
+
+    return PrecomputedMeshSource(meta, cache, config)
