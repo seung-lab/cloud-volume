540fd11f1ce4f90f81b0d06b224a1ed5fa80a04c
William Silversmith <william.silversmith@gmail.com>
2020-04-05 02:22:34 -0400

    feat: allow sharding of chunkids

--- cloudvolume/datasource/graphene/sharding.py
+++ cloudvolume/datasource/graphene/sharding.py
@@ -4,7 +4,7 @@ class GrapheneShardReader(ShardReader):
   def compute_shard_location(self, label):
     shard_loc = self.spec.compute_shard_location(label)
     chunk_id = self.meta.meta.decode_chunk_id(label)
-    filename = str(chunk_id) + '.shard'
+    filename = str(chunk_id) + '-' + str(shard_loc.shard_number) + '.shard'
     return (filename, shard_loc.minishard_number)
 
 
