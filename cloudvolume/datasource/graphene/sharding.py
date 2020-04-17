339cd0851ce022f65a3895341a7e3a825c267c3d
William Silversmith <william.silversmith@gmail.com>
2020-04-16 22:46:28 -0400

    fix(GrapheneShardReader): use correct function for chunk position

--- cloudvolume/datasource/graphene/sharding.py
+++ cloudvolume/datasource/graphene/sharding.py
@@ -3,8 +3,8 @@ from ..precomputed.sharding import ShardingSpecification, ShardReader
 class GrapheneShardReader(ShardReader):
   def compute_shard_location(self, label):
     shard_loc = self.spec.compute_shard_location(label)
-    chunk_id = self.meta.meta.decode_chunk_id_number(label)
-    filename = str(chunk_id) + '-' + str(shard_loc.shard_number) + '.shard'
+    chunk_pos_num = self.meta.meta.decode_chunk_postion_number(label)
+    filename = str(chunk_pos_num) + '-' + str(shard_loc.shard_number) + '.shard'
     return (filename, shard_loc.minishard_number)
 
 
