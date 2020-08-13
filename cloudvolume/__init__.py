6bfdaead669f342e60c31876927661da73927992
William Silversmith <william.silversmith@gmail.com>
2020-08-13 18:08:55 -0400

    Version 2.0.0

--- cloudvolume/__init__.py
+++ cloudvolume/__init__.py
@@ -68,7 +68,7 @@ from . import secrets
 from . import microviewer
 from .microviewer import view, hyperview
 
-__version__ = '1.21.0'
+__version__ = '2.0.0'
 
 # Register plugins
 from .datasource.precomputed import register as register_precomputed
