6f88d2d8763ca99d94fcd0f469ceee6224bfd2d3
William Silversmith <william.silversmith@gmail.com>
2020-09-09 19:24:09 -0400

    chore: update Trove to include py3.8

--- setup.py
+++ setup.py
@@ -63,6 +63,7 @@ setuptools.setup(
     "Programming Language :: Python :: 3.5",
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
     "Topic :: Scientific/Engineering",
     "Intended Audience :: Science/Research",
     "Operating System :: POSIX",
