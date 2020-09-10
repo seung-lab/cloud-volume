28928c9ed5088b825ca68a72503977763fd36421
William Silversmith <william.silversmith@gmail.com>
2020-09-09 23:15:02 -0400

    Version 2.1.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def requirements():
 
 setuptools.setup(
   name="cloud-volume",
-  version="2.1.0",
+  version="2.1.1",
   setup_requires=[
     'numpy<1.17; python_version<"3.5"',
     'numpy; python_version>="3.5"',
