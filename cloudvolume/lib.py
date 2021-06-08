fix(spatial_index): correct calculation of file paths (#476)

cloudvolume/lib.py
@@ -205,25 +205,23 @@ def xyzrange(start_vec, end_vec=None, stride_vec=(1,1,1)):
     end_vec = start_vec
     start_vec = (0,0,0)
 
-  start_vec = np.array(start_vec, dtype=int)
-  end_vec = np.array(end_vec, dtype=int)
+  dtype = int
+  if floating(start_vec) or floating(end_vec) or floating(stride_vec):
+    dtype= float
 
-  rangeargs = ( (start, end, stride) for start, end, stride in zip(start_vec, end_vec, stride_vec) )
-  xyzranges = [ range(*arg) for arg in rangeargs ]
-  
-  # iterate then x first, then y, then z
-  # this way you process in the xy plane slice by slice
-  # but you don't create process lots of prefix-adjacent keys
-  # since all the keys start with X
-  zyxranges = xyzranges[::-1]
+  start_vec = np.array(start_vec, dtype=dtype)
+  end_vec = np.array(end_vec, dtype=dtype)
 
-  def vectorize():
-    pt = Vec(0,0,0)
-    for z,y,x in product(*zyxranges):
-      pt.x, pt.y, pt.z = min(x, end_vec[0]), min(y, end_vec[1]), min(z, end_vec[2])
-      yield pt
-
-  return vectorize()
+  (x, y, z) = start_vec
+  while z < end_vec[2]:
+    while y < end_vec[1]:
+      while x < end_vec[0]:
+        yield Vec(x,y,z, dtype=dtype)
+        x += stride_vec[0]
+      x = start_vec[0]
+      y += stride_vec[1] 
+    y = start_vec[1]
+    z += stride_vec[2]
 
 def map2(fn, a, b):
   assert len(a) == len(b), "Vector lengths do not match: {} (len {}), {} (len {})".format(a[:3], len(a), b[:3], len(b))
@@ -254,7 +252,13 @@ def check_bounds(val, low, high):
 
 class Vec(np.ndarray):
     def __new__(cls, *args, **kwargs):
-      dtype = kwargs['dtype'] if 'dtype' in kwargs else int
+      if 'dtype' in kwargs:
+        dtype = kwargs['dtype']
+      elif floating(args):
+        dtype = float
+      else:
+        dtype = int
+
       return super(Vec, cls).__new__(cls, shape=(len(args),), buffer=np.array(args).astype(dtype), dtype=dtype)
 
     @classmethod
@@ -298,7 +302,6 @@ Vec.r = Vec.x
 Vec.g = Vec.y
 Vec.b = Vec.z
 Vec.a = Vec.w
-
 
 def floating(lst):
   return any(( isinstance(x, float) for x in lst ))

test/test_lib.py
@@ -3,7 +3,66 @@ import pytest
 import numpy as np
 
 import cloudvolume.lib as lib
-from cloudvolume.lib import Bbox, Vec
+from cloudvolume.lib import Bbox, Vec, xyzrange
+
+def test_xyzrange():
+  def xyz(*args):
+    return np.array(list(xyzrange(*args)))
+
+  assert list(xyzrange((0,0,0))) == []
+  assert list(xyzrange((1,0,0))) == []
+  assert np.all(xyz((1,1,1)) == [Vec(0,0,0)])
+  assert np.all(xyz((2,1,1)) == [Vec(0,0,0), Vec(1,0,0)])
+  assert np.all(xyz((2,2,2)) == [
+    Vec(0,0,0), Vec(1,0,0),
+    Vec(0,1,0), Vec(1,1,0),
+    Vec(0,0,1), Vec(1,0,1),
+    Vec(0,1,1), Vec(1,1,1),
+  ])
+  assert np.all(xyz((2,1,1), (3,2,2)) == [Vec(2,1,1)])
+  assert np.all(xyz((2,1,1), (5,2,2), (2,1,1)) == [Vec(2,1,1), Vec(4,1,1)])
+
+  z = xyz((0,0,0), (2,2,1), (0.5,0.5,0.5))
+  print(z)
+  print(len(z))
+
+  assert np.all(xyz((0,0,0), (2,2,1), (0.5,0.5,0.5)) == [
+    Vec(0.0,0.0,0.0),
+    Vec(0.5,0.0,0.0),
+    Vec(1.0,0.0,0.0),
+    Vec(1.5,0.0,0.0),
+    Vec(0.0,0.5,0.0),
+    Vec(0.5,0.5,0.0),
+    Vec(1.0,0.5,0.0),
+    Vec(1.5,0.5,0.0),
+    Vec(0.0,1.0,0.0),
+    Vec(0.5,1.0,0.0),
+    Vec(1.0,1.0,0.0),
+    Vec(1.5,1.0,0.0),
+    Vec(0.0,1.5,0.0),
+    Vec(0.5,1.5,0.0),
+    Vec(1.0,1.5,0.0),
+    Vec(1.5,1.5,0.0),
+    
+    Vec(0.0,0.0,0.5),
+    Vec(0.5,0.0,0.5),
+    Vec(1.0,0.0,0.5),
+    Vec(1.5,0.0,0.5),
+    Vec(0.0,0.5,0.5),
+    Vec(0.5,0.5,0.5),
+    Vec(1.0,0.5,0.5),
+    Vec(1.5,0.5,0.5),
+    Vec(0.0,1.0,0.5),
+    Vec(0.5,1.0,0.5),
+    Vec(1.0,1.0,0.5),
+    Vec(1.5,1.0,0.5),
+    Vec(0.0,1.5,0.5),
+    Vec(0.5,1.5,0.5),
+    Vec(1.0,1.5,0.5),
+    Vec(1.5,1.5,0.5),
+    
+  ])
+
 
 def test_divisors():
 