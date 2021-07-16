773feb4084ca0592b5cf0158ead375075f257bbb
William Silversmith <william.silversmith@gmail.com>
2021-07-15 22:51:06 -0400

    feat: add RANGE request support to CV neuroglancer server

--- cloudvolume/server.py
+++ cloudvolume/server.py
@@ -1,12 +1,9 @@
 import os
 
-try: 
-  from http.server import BaseHTTPRequestHandler, HTTPServer
-except ImportError:
-  from SocketServer import TCPServer as HTTPServer
-  from BaseHTTPServer import BaseHTTPRequestHandler
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 import json
+import re
 from six.moves import range
 
 import numpy as np
@@ -17,6 +14,7 @@ from cloudfiles import CloudFiles
 from cloudvolume.lib import Vec, Bbox, mkdir, save_images, yellow
 from cloudvolume.paths import ExtractedPath
 
+RANGE_REGEXP = re.compile(r'bytes=(\d+)-(\d+)')
 DEFAULT_PORT = 8080
 
 def view(cloudpath, hostname="localhost", port=DEFAULT_PORT):
@@ -39,13 +37,28 @@ class ViewerServerHandler(BaseHTTPRequestHandler):
     self.cloudpath = cloudpath
     BaseHTTPRequestHandler.__init__(self, *args)
 
+  def do_OPTIONS(self):
+    self.send_response(200, "ok")
+    self.send_header('Access-Control-Allow-Origin', '*')
+    self.send_header('Access-Control-Allow-Methods', 'GET, OPTIONS')
+    self.send_header("Access-Control-Allow-Headers", "Content-Type, Range")
+    self.end_headers()
+
   def do_GET(self):  
     if self.path.find('..') != -1:
       self.send_error(403, "Relative paths are not allowed.")
       raise ValueError("Relative paths are not allowed.")
 
+    range_header = self.headers.get("Range", None)
+
+    byte_range = (None, None)
+    if range_header:
+      byte_range = re.match(RANGE_REGEXP, range_header).groups()
+      byte_range = [ int(x) for x in byte_range ]
+
     path = self.path[1:]
-    data = CloudFiles(self.cloudpath).get(path)
+    cf = CloudFiles(self.cloudpath)
+    data = cf[path, byte_range[0]:byte_range[1]]
 
     if data is None:
       self.send_error(404, '/' + path + ": Not Found")
