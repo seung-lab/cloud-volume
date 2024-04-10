b03a19bfe52d89c8d7b7cc1fb114caa8116a1c8b
William Silversmith <william.silversmith@gmail.com>
2024-04-10 02:21:04 -0400

    feat: add warning to add_scale if non-power-of-two scale factor

--- cloudvolume/datasource/precomputed/metadata.py
+++ cloudvolume/datasource/precomputed/metadata.py
@@ -744,6 +744,15 @@ Hops:
 
     factor = np.round(factor).astype(int)
 
+    for dim in factor:
+      pot = np.log2(dim)
+      if pot != int(pot):
+        print(
+          f"WARNING: adding scale with a non-power-of-two scale factor: {factor}"
+          f"This is very uncommon and will likely cause problems at some point."
+        )
+        break
+
     # e.g. {"encoding": "raw", "chunk_sizes": [[64, 64, 64]], "key": "4_4_40", 
     # "resolution": [4, 4, 40], "voxel_offset": [0, 0, 0], 
     # "size": [2048, 2048, 256]}
