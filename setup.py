542697f80978c5b9946c17a50799eea13dfbadc6
William Silversmith <william.silversmith@gmail.com>
2024-06-11 22:11:52 -0400

    fix: syntax error

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setuptools.setup(
     "Development Status :: 5 - Production/Stable",
     "License :: OSI Approved :: BSD License",
     "Programming Language :: Python",
-    "Programming Language :: Python :: 3",,
+    "Programming Language :: Python :: 3",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3.10",
