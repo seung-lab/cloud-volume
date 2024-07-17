afc285a987ee871ae7bfad7ab8538e1ff2c6d616
William Silversmith <william.silversmith@gmail.com>
2024-07-17 17:28:28 -0400

    feat(mesh/viewer): upgrade mesh viewer to full 3d surface display

--- cloudvolume/mesh.py
+++ cloudvolume/mesh.py
@@ -408,74 +408,60 @@ end_header
 
     return self.deduplicate_vertices(is_chunk_aligned)
 
+
   def viewer(self):
+    # thanks to ChatGPT for making it easy to figure out
+    # how to display VTK meshes.
     try:
       import vtk
     except ImportError:
       print("The mesh viewer requires the OpenGL based vtk. Try: pip install vtk --upgrade")
       return
+    polydata = self._create_vtk_mesh(self.vertices, self.faces)
 
-    colors = vtk.vtkNamedColors()
-    # Set the background color.
-    bkg = map(lambda x: x / 255.0, [40, 40, 40, 255])
-    colors.SetColor("BkgColor", *bkg)
-
-    # This creates a point cloud model
-    points = vtk.vtkPoints()
-    verts = vtk.vtkCellArray()
-    polydata = vtk.vtkPolyData()
-    polydata.SetPoints(points)
-    polydata.SetVerts(verts)
-    for vertex in self.vertices:
-      pid = points.InsertNextPoint(vertex)
-      verts.InsertNextCell(1)
-      verts.InsertCellPoint(pid)
-
-    # The mapper is responsible for pushing the geometry into the graphics
-    # library. It may also do color mapping, if scalars or other
-    # attributes are defined.
     mapper = vtk.vtkPolyDataMapper()
     mapper.SetInputData(polydata)
 
-    # The actor is a grouping mechanism: besides the geometry (mapper), it
-    # also has a property, transformation matrix, and/or texture map.
-    # Here we set its color and rotate it -22.5 degrees.
-    cylinderActor = vtk.vtkActor()
-    cylinderActor.SetMapper(mapper)
-    cylinderActor.GetProperty().SetColor(colors.GetColor3d("Mint"))
-    cylinderActor.RotateX(30.0)
-    cylinderActor.RotateY(-45.0)
+    actor = vtk.vtkActor()
+    actor.SetMapper(mapper)
 
-    # Create the graphics structure. The renderer renders into the render
-    # window. The render window interactor captures mouse events and will
-    # perform appropriate camera or actor manipulation depending on the
-    # nature of the events.
-    ren = vtk.vtkRenderer()
-    renWin = vtk.vtkRenderWindow()
-    renWin.AddRenderer(ren)
-    iren = vtk.vtkRenderWindowInteractor()
-    iren.SetRenderWindow(renWin)
+    renderer = vtk.vtkRenderer()
+    render_window = vtk.vtkRenderWindow()
+    render_window.AddRenderer(renderer)
+    render_window_interactor = vtk.vtkRenderWindowInteractor()
+    render_window_interactor.SetRenderWindow(render_window)
 
-    # Add the actors to the renderer, set the background and size
-    ren.AddActor(cylinderActor)
-    ren.SetBackground(colors.GetColor3d("BkgColor"))
-    renWin.SetSize(800, 800)
+    render_window.SetSize(1024, 1024)
 
-    text = "Point Cloud Rendering of Mesh Object"
+    renderer.AddActor(actor)
+    renderer.SetBackground(0.1, 0.2, 0.3)  # Background color
+
+    text = "Mesh Viewer"
     if self.segid is not None:
-      renWin.SetWindowName(text + " (Label {})".format(self.segid))
+      render_window.SetWindowName(text + " (Label {})".format(self.segid))
     else:
-      renWin.SetWindowName(text)
+      render_window.SetWindowName(text)
 
-    # This allows the interactor to initalize itself. It has to be
-    # called before an event loop.
-    iren.Initialize()
+    render_window.Render()
+    render_window_interactor.Start()
 
-    # We'll zoom in a little by accessing the camera and invoking a "Zoom"
-    # method on it.
-    ren.ResetCamera()
-    ren.GetActiveCamera().Zoom(1.5)
-    renWin.Render()
+  def _create_vtk_mesh(self, vertices, faces):
+    import vtk
+    from vtk.util.numpy_support import numpy_to_vtk, numpy_to_vtkIdTypeArray
 
-    # Start the event loop.
-    iren.Start()    
+    vtk_points = vtk.vtkPoints()
+    vtk_points.SetData(numpy_to_vtk(vertices))
+    
+    polydata = vtk.vtkPolyData()
+    polydata.SetPoints(vtk_points)
+    
+    vtk_faces = vtk.vtkCellArray()
+    vtk_faces.SetCells(
+      faces.shape[0], 
+      numpy_to_vtkIdTypeArray(np.hstack([np.full((faces.shape[0], 1), 3), faces]).flatten())
+    )
+
+    polydata.SetPolys(vtk_faces)
+    
+    return polydata
+
