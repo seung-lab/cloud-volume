0da4f4b09863155fafece2dd8e2fa4b9329d18fe
William Silversmith <william.silversmith@gmail.com>
2025-02-26 18:59:48 -0500

    Version 12.0.0

--- cloudvolume/__init__.py
+++ cloudvolume/__init__.py
@@ -65,7 +65,7 @@ from .skeleton import Skeleton, PrecomputedSkeleton
 from . import exceptions
 from . import secrets
 
-__version__ = '11.2.0'
+__version__ = '12.0.0'
 
 # Register plugins
 from .datasource.precomputed import register as register_precomputed
--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ OTHER_FORMATS = [
 
 setuptools.setup(
   name="cloud_volume",
-  version="11.2.0",
+  version="12.0.0",
   setup_requires=[
     'numpy<1.17; python_version<"3.5"',
     'numpy; python_version>="3.5"',
