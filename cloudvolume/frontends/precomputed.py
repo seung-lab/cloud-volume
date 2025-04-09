5030279a7d8ea8c4e0343f0dfe0bd1e4af633267
William Silversmith <william.silversmith@gmail.com>
2025-04-09 18:34:09 -0400

    fix: try just letting __del__ handle things

--- cloudvolume/frontends/precomputed.py
+++ cloudvolume/frontends/precomputed.py
@@ -1189,8 +1189,8 @@ class CloudVolumePrecomputed(object):
       order=order,
       use_file=True,
     )
-    del shared_image
-    mmap_handle.close()
+    # del shared_image
+    # mmap_handle.close()
 
   def viewer(self, port=1337):
     import cloudvolume.server
