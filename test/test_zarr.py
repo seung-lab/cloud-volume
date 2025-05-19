8d1cb29d0f4568a81138c068a4c1b39b0c74a569
William Silversmith <william.silversmith@gmail.com>
2025-05-19 17:41:58 -0400

    test: simple tests working

--- test/test_zarr.py
+++ test/test_zarr.py
@@ -13,16 +13,23 @@ TEST_DIR = os.path.dirname("/tmp/removeme/cloudvolume/")
 def test_zarr3_unsharded():
 	test_location = os.path.join(TEST_DIR, "zarr_unsharded.zarr")
 
-	shape = [1000, 1000, 50]
-	data = np.zeros(shape, dtype=np.uint8)
+	shape = [100, 100, 50]
+	data = np.zeros(shape, dtype=np.uint8, order="C")
 	data[:20] = 1
 
-	arr = zarr.open(store=test_location, shape=shape, chunks=(10, 10, 10), dtype='uint8', mode='w')
+	arr = zarr.open(store=test_location, shape=shape, chunks=(100, 100, 10), dtype='uint8', mode='w')
 	arr[:] = data
 	arr.store.close()
 
 	cv = CloudVolume(f"zarr3://file://{test_location}", fill_missing=True)
 
+	# by default zarr3 in C order [z,y,x], so inverting axes
+	# results in a full transposition [x,y,z]
 	assert np.all(cv[:][...,0] == data.T)
 
+	cv[:] = 2
+
+	arr = zarr.open(store=test_location, shape=shape, chunks=(100, 100, 10), dtype='uint8', mode='r')
+	assert np.all(arr[:] == 2)
+	assert np.all(cv[:] == 2)
 
