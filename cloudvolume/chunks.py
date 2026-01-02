526394ed165634f48bd9ff996e6fec2fa8bfff38
Dodam Ih <dodam@zetta.ai>
2026-01-02 06:13:55 -0800

    feat: raw encoding optimisation

--- cloudvolume/chunks.py
+++ cloudvolume/chunks.py
@@ -321,6 +321,8 @@ def encode_compressed_segmentation(
   return cseg.compress(subvol, block_size=block_size, order=order)
 
 def encode_raw(subvol):
+  if not subvol.flags['F_CONTIGUOUS']:
+    subvol = np.asfortranarray(subvol)
   return subvol.tobytes('F')
 
 def encode_kempressed(subvol):
