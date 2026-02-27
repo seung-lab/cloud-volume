a4b5b486dd8d32db8fb26f3070338fba89c2fb67
William Silversmith <william.silversmith@gmail.com>
2026-02-27 13:29:10 -0500

    Version 12.11.0

--- cloudvolume/__init__.py
+++ cloudvolume/__init__.py
@@ -65,7 +65,7 @@ from .skeleton import Skeleton, PrecomputedSkeleton
 from . import exceptions
 from . import secrets
 
-__version__ = '12.10.0'
+__version__ = '12.11.0'
 
 # Register plugins
 from .datasource.precomputed import (
