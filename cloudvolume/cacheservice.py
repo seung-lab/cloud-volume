seung-lab/cloud-volume

commit 29c52e91a57832195c1fce119adc3128becc3c61

perf: faster string processing for multilod meshes

cloudvolume/cacheservice.py
@@ -591,7 +591,10 @@ class CacheService(object):
     def no_compression_ext(fnames):
       results = []
       for fname in fnames:
-        (name, ext) = pathmodule.splitext(fname)
+        dot = fname.rfind(".", -10)
+        if dot == -1:
+          continue
+        (name, ext) = fname[:dot], fname[dot:]
         if ext in COMPRESSION_EXTENSIONS:
           results.append(name)
         else:

commit a799475287a30666036c3d4d7f121a81f18782db

fix(cache): broken behavior in caching with no extension
Code was broken in 29c52e91a57832195c1fce119adc3128becc3c61

cloudvolume/cacheservice.py
@@ -26,6 +26,7 @@ def no_compression_ext(list_dir:str):
   for fname in fnames:
     dot = fname.rfind(".", -10)
     if dot == -1:
+      results.append(fname)
       continue
     (name, ext) = fname[:dot], fname[dot:]
     if ext in COMPRESSION_EXTENSIONS: