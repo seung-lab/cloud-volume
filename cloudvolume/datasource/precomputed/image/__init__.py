9c98e9aaf4f77d9aba0dd891e73b05f218e0a412
William Silversmith <william.silversmith@gmail.com>
2026-07-16 02:49:22 -0400

    fix: handle shards with truncated edges in make_shard_chunks

--- cloudvolume/datasource/precomputed/image/__init__.py
+++ cloudvolume/datasource/precomputed/image/__init__.py
@@ -778,9 +778,8 @@ class PrecomputedImageSource(ImageSourceInterface):
     pt_anchor = gpts[0] * chunk_size
     for pt_abs, morton_code in zip(gpts, morton_codes):
       cutout_bbx = Bbox(pt_abs * chunk_size, (pt_abs + 1) * chunk_size)
-      cutout_bbx -= pt_anchor
-
       cutout_bbx.maxpt = cutout_bbx.maxpt.clip(None, self.meta.volume_size(mip))
+      cutout_bbx -= pt_anchor
 
       chunk = img[ cutout_bbx.to_slices() ]
 
