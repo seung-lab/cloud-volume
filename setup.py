38e593e04144dd704a1e8242e2c109e627366f45
William Silversmith <william.silversmith@gmail.com>
2026-09-18 15:55:15 -0400

    Version 12.15.0

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ ZARR_CODECS = [
 
 setuptools.setup(
   name="cloud_volume",
-  version="12.14.4",
+  version="12.15.0",
   setup_requires=[
     'numpy<1.17; python_version<"3.5"',
     'numpy; python_version>="3.5"',
